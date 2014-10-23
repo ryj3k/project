34dbf3b7610f8a9a6332a82d7bf51349ccdd3321
Grzegorz Sochacki <ryj3k.cacki@gmail.com>
2014-10-23 18:43:25 +0200

    First

---
+++ Exam-System-ejb/src/java/test/TestBean.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package test;
+
+import javax.ejb.Stateless;
+import javax.ejb.LocalBean;
+
+/**
+ *
+ * @author ryj3k
+ */
+@Stateless
+@LocalBean
+public class TestBean {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
